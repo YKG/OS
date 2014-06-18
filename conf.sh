0bb45e3bf92f0dfc6f762ab46930059d6f6df850
YKG <yekaige2010@gmail.com>
2014-06-18 11:54:18 +0800

    ch3_c

--- conf.sh
+++ conf.sh
@@ -1,5 +1,5 @@
-nasm -o pm2.com pm2.asm
+nasm -o ldt.com ldt.asm
 sudo mount -o loop a.img /mnt/floppy
-sudo cp pm2.com /mnt/floppy
+sudo cp ldt.com /mnt/floppy
 sudo umount /mnt/floppy
 
--- ldt.asm
+++ ldt.asm
@@ -11,11 +11,13 @@ Code32_DESC:	Descriptor	0, Code32Len - 1, DA_C + DA_32
 Code16_DESC:	Descriptor	0, 0ffffh, DA_C
 VIDEO_DESC:	Descriptor	0b8000h, 0ffffh, DA_DRW
 Normal_DESC:	Descriptor	0, 0ffffh, DA_DRW
+LDT_DESC:	Descriptor	0, LDT_LEN - 1, DA_LDT
 
 SelectorCode32	equ	Code32_DESC - GDT_DESC
 SelectorCode16	equ	Code16_DESC - GDT_DESC
 SelectorVideo	equ	VIDEO_DESC - GDT_DESC
 SelectorNormal	equ	Normal_DESC - GDT_DESC
+SelectorLDT	equ	LDT_DESC - GDT_DESC
 
 GdtLen	equ	$ - $$
 GdtPtr	dw	GdtLen - 1
@@ -44,6 +46,26 @@ mov	[Code32_DESC + 2], ax
 shr	eax, 16
 mov	[Code32_DESC + 4], al
 mov	[Code32_DESC + 7], ah
+
+
+xor	eax, eax
+mov	ax, cs
+shl	eax, 4
+add	eax, LABEL_LDT
+mov	[LDT_DESC + 2], ax
+shr	eax, 16
+mov	[LDT_DESC + 4], al
+mov	[LDT_DESC + 7], ah
+
+
+xor	eax, eax
+mov	ax, cs
+shl	eax, 4
+add	eax, LABEL_SEG_CODEA
+mov	[CodeA_DESC + 2], ax
+shr	eax, 16
+mov	[CodeA_DESC + 4], al
+mov	[CodeA_DESC + 7], ah
 
 
 xor	eax, eax
@@ -101,12 +123,12 @@ int	21h
 ;ALIGN 32
 [BITS 16]
 LABEL_SEG_CODE16:
-;mov	ax, SelectorNormal
-;mov	ds, ax
-;mov	es, ax
-;mov	ss, ax
-;mov	gs, ax
-;mov	fs, ax
+mov	ax, SelectorNormal
+mov	ds, ax
+mov	es, ax
+mov	ss, ax
+mov	gs, ax
+mov	fs, ax
 
 mov	eax, cr0
 and	al, 11111110b
@@ -118,23 +140,8 @@ mov	cr0, eax
 ;mov	edi, 02eh
 ;mov	al, [cs:edi]
 
-mov	ax, 3224h
-mov	ds, ax
-mov	es, ax
-mov	ss, ax
-
-mov	sp, [SP_IN_REAL_MODE]
-
-in	al, 92h
-and	al, 11111101b
-out	92h, al
-
-sti
-mov	ax, 4c00h
-int	21h
 
 LABEL_GO_BACK_TO_REAL:
-;mov	ax, 0
 jmp	0:LABEL_REAL_ENTRY
 
 Code16Len	equ $ - $$
@@ -155,7 +162,41 @@ mov	ah, 0ch
 mov	al, 'P'
 mov	[gs:edi], ax
 
+mov	ax, SelectorLDT
+lldt	ax
+
+jmp	SelectorCodeA:0
+
 jmp	SelectorCode16:0
 jmp	$
 
 Code32Len equ	$ - $$
+
+
+
+[SECTION .ldt]
+LABEL_LDT:
+CodeA_DESC:	Descriptor	0, CodeA_Len - 1, DA_C + DA_32 
+
+SelectorCodeA	equ	CodeA_DESC - LABEL_LDT + SA_TIL
+
+LDT_LEN		equ	$ - $$
+
+
+
+[SECTION .ldt32]
+[BITS 32]
+LABEL_SEG_CODEA:
+mov	ax, SelectorVideo
+mov	gs, ax
+mov	edi, (80*14 + 79)*2
+mov	ah, 0ch
+mov	al, 'L'
+mov	[gs:edi], ax
+
+jmp	SelectorCode16:0
+jmp	$
+
+CodeA_Len equ	$ - $$
+
+
